add54f4906ac180a11a5a70f403995317b08bf50
novb1492 <novb1492@naver.com>
2021-08-07 00:23:44 +0900

    성공 했는데 찜찜하게 성공..

--- src/reservation/mainController.java
+++ src/reservation/mainController.java
@@ -12,10 +12,11 @@ import javafx.scene.control.Label;
 public class mainController  implements Initializable  {
 	
 	private Parent parent;
+	private Parent parent2;
+	private String ab;
 	private reservationService reservationService;
+	int day;
 
-	@FXML private Button day1;
-	@FXML private Button day2;
 
 
 	
@@ -23,22 +24,34 @@ public class mainController  implements Initializable  {
 	public void setParent(Parent parent) {
 		this.parent=parent;
 	}
+	public void setParent2(Parent parent,Parent parent2,int day) {
+		this.parent=parent;
+		this.parent2=parent2;
+		this.day=day;
+	}
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		reservationService=new reservationService();
-		day1.setOnMouseClicked((event)->{
-			reservationService.setDate(1);
-			reservationService.showTimePage(parent);
-		});
-		day2.setOnMouseClicked((event)->{
-			reservationService.setDate(2);
-		});
 	}
-	
+	public void click() {
+		System.out.println("1");
+		day=1;
+	}
+	public void click2() {
+		System.out.println("2");
+		day=2;
+	}
 	public void insert() {
-		reservationService.insert(parent,"kim@kim.com","kim");
+		System.out.println("insert"+ab);
+		reservationService.insert(parent,"kim@kim.com","kim",parent2,day);
 	}
+	public void show() {
+		System.out.println("show");
+		System.out.println(day);
+		reservationService.showTimePage(parent,day);
+	}
+
 
 
 }
--- src/reservation/reservationService.java
+++ src/reservation/reservationService.java
@@ -5,45 +5,54 @@ package reservation;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 
 public class reservationService  {
+	//@FXML private Button insert;
+	
 	private int choiceDay;
 	private reservationDao reservationDao=new reservationDao();
 	public void setDate(int choiceDay) {
 		this.choiceDay=choiceDay;
 	}
-	public void insert(Parent parent,String email,String name) {
-		if(choiceDay==0&&choiceDay>31) {
-			System.out.println("잘못된 날짜 선택입니다");
-			return;
-		}
-		Label month=(Label)parent.lookup("#month");
-		System.out.println(month.getText());
-		System.out.println(choiceDay);
-		
-		int time=5;
+	public void insert(Parent parent,String email,String name,Parent parent2,int day) {
+		try {
+			if(day==0&&day>31) {
+				System.out.println("잘못된 날짜 선택입니다");
+				return;
+			}
+			Label month=(Label)parent.lookup("#month");
+			System.out.println(month.getText());
+			System.out.println(day);
+			
+			int time=5;
 
-		
-		Timestamp rDate=Timestamp.valueOf("2021-"+month.getText()+"-"+choiceDay+" "+time+":00:00");
-		System.out.println(rDate);
-		
-		Timestamp created=Timestamp.valueOf(LocalDateTime.now());
-		System.out.println(created);
-		reservationDto reservationDto=new reservationDto();
-		reservationDto.setEmail(email);
-		reservationDto.setName(name);
-		reservationDto.setCreated(created);
-		reservationDto.setTime(time);
-		reservationDto.setrDate(rDate);
-		reservationDao.insert(reservationDto);
+			
+			Timestamp rDate=Timestamp.valueOf("2021-"+month.getText()+"-"+day+" "+time+":00:00");
+			System.out.println(rDate);
+			
+			Timestamp created=Timestamp.valueOf(LocalDateTime.now());
+			System.out.println(created);
+			reservationDto reservationDto=new reservationDto();
+			reservationDto.setEmail(email);
+			reservationDto.setName(name);
+			reservationDto.setCreated(created);
+			reservationDto.setTime(time);
+			reservationDto.setrDate(rDate);
+			reservationDao.insert(reservationDto);
+		} catch (Exception e) {
+			e.printStackTrace();
+			}
+			
 	}
-	public void showTimePage(Parent parent) {
+	public void showTimePage(Parent parent,int day) {
 		System.out.println("showTimePage");
 		FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("showTimePage.fxml"));
 		try {
@@ -51,6 +60,12 @@ public class reservationService  {
 			Stage stage=new Stage();
 			stage.setScene(new Scene(parent2));
 			stage.show();
+			
+		
+		
+			mainController mainController=fxmlLoader.getController();
+			mainController.setParent2(parent,parent2,day);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
